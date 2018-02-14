bf828b756b0765f45c1567019ddd496792224b16
William Lee <18326007+williamyuhanglee@users.noreply.github.com>
2018-02-14 14:56:15 -0500

    Modified Side.

--- src/Side.java
+++ src/Side.java
@@ -1,3 +1,15 @@
 enum Side {
-    RED, BLACK, NONE
+    RED, BLACK, NONE;
+
+    Piece[] pieces = new Piece[8];
+    int count = 0;
+
+    void addPiece(Piece piece) {
+        pieces[count] = piece;
+        count++;
+    }
+
+    void pieceKilled() {
+        count--;
+    }
 }
