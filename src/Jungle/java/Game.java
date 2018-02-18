c9cc46461d4e126472dc1c364e29f44908debf0d
William Lee <18326007+williamyuhanglee@users.noreply.github.com>
2018-02-17 19:47:05 -0500

    Improved user interaction.

--- src/Jungle/java/Game.java
+++ src/Jungle/java/Game.java
@@ -9,6 +9,10 @@ class Game {
     static void launch() {
         startUp();
         gameIntro();
+        System.out.println("\nPress ENTER key to start the game.");
+        try {
+            System.in.read();
+        } catch (Exception e) {}
         startGame();
     }
 
@@ -23,33 +27,41 @@ class Game {
         System.out.println("===================================");
         System.out.println("||          Developed by         ||");
         System.out.println("||     William Lee & Frank Lu    ||");
+        System.out.println();
     }
 
     private static void gameIntro() {
         System.out.println("================================================================================");
         System.out.println("                                  Introduction                                  ");
         System.out.println("================================================================================");
-        System.out.println("Welcome to the Jungle!");
-        System.out.println(WordUtils.wrap("Command your animals to capture all the other player's animals or their den (marked with \"O\") to win!", 80));
-        System.out.println(WordUtils.wrap("Animals on the red side are marked with CAPITAL letters, and small letters on the black side.", 80));
+        System.out.println("Welcome to the Jungle!\n");
+        System.out.println(WordUtils.wrap("Command your animals to capture all the other player's animals or their den to win!\n", 80));
+        System.out.println("Game rules:");
+        System.out.println("The Jungle game board consists of 7 columns and 9 rows of squares.");
+        System.out.println("Animals of two sides, red and black, move on the square spaces.");
+        System.out.println(WordUtils.wrap("Animals on the red side are marked with CAPITAL letters, and animals on the black side with small letters.", 80));
         System.out.println(WordUtils.wrap("Starting from red, each turn a player can move one of their animals by one square horizontally or vertically (a special rule for Lions and Tigers will be explained later).", 80));
-        System.out.println(WordUtils.wrap("Higher ranking animals can capture lower or identical ranking animals by moving into their square, except for Rats, which can capture Elephants and Elephants can't capture Rats.", 80));
-        System.out.println(WordUtils.wrap("The animal ranking, from highest to lowest, along with their markers in the game is:", 80));
-        System.out.println("Rank|Marker|Animal");
-        System.out.println("  8    E    Elephant");
-        System.out.println("  7    L    Lion");
-        System.out.println("  6    T    Tiger");
-        System.out.println("  5    J    Jaguar");
-        System.out.println("  4    W    Wolf");
-        System.out.println("  3    D    Dog");
-        System.out.println("  2    C    Cat");
-        System.out.println("  1    R    Rat");
-        System.out.println(WordUtils.wrap("There are three traps (marked with \"XXX\") placed next to each side's den. You can capture any enemy animals in your dens using any animals regardless of rank.", 80));
+        System.out.println(WordUtils.wrap("Higher ranking animals can capture LOWER OR IDENTICAL ranking animals by moving into their square, except for Rats, which can capture Elephants and Elephants can't capture Rats.", 80));
+        System.out.println(WordUtils.wrap("The animal ranking, from highest to lowest, along with animal markers in the game is:", 80));
+        printRanking();
+        System.out.println("(you can type \"ranking\" in the game to show this again)");
+        System.out.println(WordUtils.wrap("There are three traps (marked with \"XXX\") placed next to each side's den. You can capture any enemy animals in your dens using any of your animals regardless of rank.", 80));
         System.out.println(WordUtils.wrap("Two areas of water (marked with \"~~~\") are located in the center of the board. Only Rats and Dogs may enter the water and engage other animals in the water.", 80));
         System.out.println(WordUtils.wrap("However, they can't enter a water square from land or a land square from water if there is another animal (of any side) on that square.", 80));
-        System.out.println(WordUtils.wrap("Lions and Tigers may jump over the water horizontally or vertically, but they can't do so if there is any other animal on their path.", 80));
-        System.out.println(WordUtils.wrap("Finally, animals may not move into their own den. If an enemy animal moves into the den, the player loses.", 80));
-        //System.out.println("================================================================================");
+        System.out.println(WordUtils.wrap("Lions and Tigers may jump over the water horizontally or vertically, but they can't do so if there is any other animal in their path.", 80));
+        System.out.println(WordUtils.wrap("Finally, animals may not move into their own den (marked with \"O\"). If an enemy animal moves into a player's den, the player loses.", 80));
+    }
+
+    private static void printRanking() {
+        System.out.println("Rank|Marker|Animal");
+        System.out.println("  8   E/e   Elephant");
+        System.out.println("  7   L/l   Lion");
+        System.out.println("  6   T/t   Tiger");
+        System.out.println("  5   J/j   Jaguar");
+        System.out.println("  4   W/w   Wolf");
+        System.out.println("  3   D/d   Dog");
+        System.out.println("  2   C/c   Cat");
+        System.out.println("  1   R/r   Rat");
     }
 
     private static void startGame() {
@@ -70,10 +82,15 @@ class Game {
                 boolean validAnimal = false;
                 String chosenAnimal;
                 Piece chosenPiece = null;
+                Locating:
                 while (!validAnimal) {
                     System.out.println("(Choose from" + playersTurn.printAlivePieces() + ")");
                     System.out.print("Please select an animal to move: ");
                     chosenAnimal = sc.nextLine();
+                    if (chosenAnimal.equals("ranking")) {
+                        printRanking();
+                        continue Locating;
+                    }
                     if (chosenAnimal.equals("E") || chosenAnimal.equals("e") ||
                             chosenAnimal.equals("L") || chosenAnimal.equals("l") ||
                             chosenAnimal.equals("T") || chosenAnimal.equals("t") ||
@@ -118,6 +135,9 @@ class Game {
                     String chosenCoordinates = sc.nextLine();
                     if (chosenCoordinates.equals("reselect")) {
                         continue Turn;
+                    } else if (chosenCoordinates.equals("ranking")) {
+                        printRanking();
+                        continue Targeting;
                     } else {
                         for (Square square: chosenPiece.accessibleSquares()) {
                             if (chosenCoordinates.equals(square.printCoordinates())) {
